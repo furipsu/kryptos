0545d2261320708626b9342ddd187af253b242b5
furipsu <66990266+furipsu@users.noreply.github.com>
2020-06-30 03:12:45 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+ 
+    var ancho = $(window).width();
+ 
+	if (ancho <= 1350){
+		$('body').css({
+			'background-size': 'initial'
+		});
+	}
+
+	$(window).scroll(function(){
+		var barra = $(window).scrollTop();
+		var posicion =  (barra * 0.10);
+		
+		$('body').css({
+			'background-position': '0 -' + posicion + 'px'
+		});
+ 
+	});
+ 
+});
